66802e5b2b87bd8d7204d5f65567ee3d1a2e3687
gFon380 <glf2121@columbia.edu>
2024-04-16 21:06:48 -0400

    Added start screen and learning modules.

--- app.py
+++ app.py
@@ -1,10 +1,46 @@
 from flask import Flask, render_template, request, jsonify
 
+learningmodulesdata = {
+    "1": {
+        "id": 1,
+        "title": "Learn 1",
+        "completed": "false",
+    },
+        "2": {
+        "id": 2,
+        "title": "Learn 2",
+        "completed": "false",
+    },
+        "3": {
+        "id": 3,
+        "title": "Learn 3",
+        "completed": "false",
+    },
+        "4": {
+        "id": 4,
+        "title": "Learn 4",
+        "completed": "false",
+    },
+}
+
+#When done with lesson four, change this variable to true.
+allModulesComplete = False
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', data=allModulesComplete)
+
+@app.route('/learningmodules')
+def learningmodules():
+    print(learningmodules)
+    print(type(learningmodules))
+    return render_template('learningmodules.html', data=learningmodulesdata)
+
+@app.route('/learn/<id>')
+def learn(id):
+    return render_template('learn.html')
 
 @app.route('/about')
 def about():
