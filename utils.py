9d4cdd934ad55374a59a73625da9ffd7b18be2de
bill <billsun9@gmail.com>
2024-04-24 10:30:43 -0700

    updates to quiz

--- utils.py
+++ utils.py
@@ -2,8 +2,8 @@ QUIZ_PARAMS = {
     "easy": [
         {"id": 0, "name": "dahlia", "url": "bees/dahlia.jpg", "gt": "bees", "nxt_id": 1},
         {"id": 1, "name": "fennel", "url": "bees/fennel.jpg", "gt": "bees", "nxt_id": 2},
-        {"id": 2, "name": "milkweed", "url": "butterflys/milkweed.jpg", "gt": "butterflies", "nxt_id": 3},
+        {"id": 2, "name": "milkweed", "url": "butterflies/milkweed.jpg", "gt": "butterflies", "nxt_id": 3},
         {"id": 3, "name": "goldenrod", "url": "bats/goldenrod.jpg", "gt": "bats", "nxt_id": "4"},
-        {"id": 4, "name": "passionflower", "url": "butterflys/passionflower.jpeg", "gt": "butterflies", "nxt_id": "submit"},
+        {"id": 4, "name": "passionflower", "url": "butterflies/passionflower.jpeg", "gt": "butterflies", "nxt_id": "submit"},
     ]
 }
