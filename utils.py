be489742529c2761728d4c18f1ec6911d633d385
bill <billsun9@gmail.com>
2024-04-23 20:12:50 -0700

    modified flowers

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 QUIZ_PARAMS = {
     "easy": [
-        {"id": 0, "name": "rose", "url": "rose.jpg", "gt": "bee", "nxt_id": 1},
-        {"id": 1, "name": "tulip", "url": "tulip.jpg", "gt": "bird", "nxt_id": 2},
-        {"id": 2, "name": "peony", "url": "peony.jpg", "gt": "bird", "nxt_id": "submit"},
+        {"id": 0, "name": "dahlia", "url": "bees/dahlia.jpg", "gt": "bees", "nxt_id": 1},
+        {"id": 1, "name": "fennel", "url": "bees/fennel.jpg", "gt": "bees", "nxt_id": 2},
+        {"id": 2, "name": "milkweed", "url": "butterflys/milkweed.jpg", "gt": "butterflies", "nxt_id": 3},
+        {"id": 3, "name": "goldenrod", "url": "bats/goldenrod.jpg", "gt": "bats", "nxt_id": "4"},
+        {"id": 4, "name": "passionflower", "url": "butterflys/passionflower.jpeg", "gt": "butterflies", "nxt_id": "submit"},
     ]
 }
